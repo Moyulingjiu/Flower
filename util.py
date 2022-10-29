26876f404b838133215f006d4f7597d8870f65e8
墨羽翎玖 <1597867839@qq.com>
2022-10-29 11:46:43 +0800

    修复无法正确显示中奖名单的问题

--- util.py
+++ util.py
@@ -1149,7 +1149,6 @@ def complete_lottery() -> None:
     # 每天系统会补加30金币
     system_data.lottery_prize_pool -= bonus * len(lottery_list) - 3000
     system_data.winning_lottery_info = '中奖名单（上一期中奖数字：%d）：' % rand
-    flower_dao.update_system_data(system_data)
     index = 1
     for lottery in lottery_list:
         lottery.winning = True
@@ -1169,6 +1168,7 @@ def complete_lottery() -> None:
             index += 1
         except ResBeLockedException:
             continue
+    flower_dao.update_system_data(system_data)
 
 
 def lock_the_world() -> None:
